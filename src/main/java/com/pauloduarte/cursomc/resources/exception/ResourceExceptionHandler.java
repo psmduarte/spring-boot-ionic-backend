ae27e6c74c3bc10f70a5a05f06316973421038e9
Paulo Duarte <psmduarte@live.com>
2018-04-24 19:01:09 +0100

    Apagar categoria com DELETE

--- src/main/java/com/pauloduarte/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/pauloduarte/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.pauloduarte.cursomc.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.pauloduarte.cursomc.services.exceptions.DataIntegrityException;
 import com.pauloduarte.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,6 +20,12 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+	
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(),e.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 		
 
 }
