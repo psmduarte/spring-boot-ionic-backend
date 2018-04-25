1486a1dc894fd743c3698b94af96642497d8664c
Paulo Duarte <psmduarte@live.com>
2018-04-25 22:43:01 +0100

    Validacao sintactica com pacote Bean Validation

--- src/main/java/com/pauloduarte/cursomc/services/CategoriaService.java
+++ src/main/java/com/pauloduarte/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pauloduarte.cursomc.domain.Categoria;
+import com.pauloduarte.cursomc.dto.CategoriaDTO;
 import com.pauloduarte.cursomc.repositories.CategoriaRepository;
 import com.pauloduarte.cursomc.services.exceptions.DataIntegrityException;
 import com.pauloduarte.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService{
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
