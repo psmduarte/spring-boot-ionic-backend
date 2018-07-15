9009d736512ecbc2bf2d14936dd4e6cb286921e8
Paulo Duarte <psmduarte@live.com>
2018-07-15 20:16:27 +0100

    Implementacao da autenticacao e criacao do token JWT

---
+++ src/main/java/com/pauloduarte/cursomc/security/UserSS.java
@@ -0,0 +1,82 @@
+package com.pauloduarte.cursomc.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.pauloduarte.cursomc.domain.enums.Perfil;
+
+public class UserSS implements UserDetails {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public UserSS() {
+	}
+	
+
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList()); //Para cada perfil x da minha lista de perfis vou dar um SimpleGrantAuthority pegando no String correspondente ao Perfil
+	}
+
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+	
+	public Integer getIde() {
+		return id;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
