8467a621feb1b3d0f048554db45c7f542a06d72a
Paulo Duarte <psmduarte@live.com>
2018-07-25 18:32:22 +0100

    Implementacao da autorizacao

---
+++ src/main/java/com/pauloduarte/cursomc/security/JWTAuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.pauloduarte.cursomc.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+	
+	private JWTUtil jwtUtil;
+	
+	private UserDetailsService userDetailsService;
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager,JWTUtil jwtUtil,UserDetailsService userDetailsService ) {
+		super(authenticationManager);
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, 
+									HttpServletResponse response, 
+									FilterChain chain) throws IOException, ServletException{
+		
+		String header = request.getHeader("Authorization");
+		this.jwtUtil = jwtUtil;
+		
+		if(header != null && header.startsWith("Bearer")) {
+			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(6)); // 6 porque considera toda a String menos os primeiros 6 caracteres (palavra Bearer)
+			if(auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+		if(jwtUtil.tokenValido(token)) {
+			String username = jwtUtil.getUsername(token);
+			UserDetails user = userDetailsService.loadUserByUsername(username);
+			return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		}
+		return null;
+	}
+	
+	
+
+}
