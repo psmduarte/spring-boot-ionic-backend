5b9c64fb2337609cbbbe98000b09e3b09f0ebf60
Paulo Duarte <psmduarte@live.com>
2018-08-12 17:34:04 +0100

    Guardado o primeiro arquivo no bucket do S3

---
+++ src/main/java/com/pauloduarte/cursomc/services/S3Service.java
@@ -0,0 +1,45 @@
+package com.pauloduarte.cursomc.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+	
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3Client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadFile(String localFilePath) {
+		try {
+			LOG.info("Inicio de Upload");
+			File file = new File(localFilePath);
+			s3Client.putObject(new PutObjectRequest(bucketName, "teste.jpg", file));
+			LOG.info("Upload Concluido");
+			
+		}
+		catch(AmazonServiceException e) {
+			LOG.info("AmazonServiceException: " + e.getErrorMessage());
+			LOG.info("SatusCode: " + e.getErrorCode());
+		}
+		
+		catch(AmazonClientException e) {
+			LOG.info("AmazonClienteException: " + e.getMessage());
+		}
+	}
+
+}
