fdccc611e301e9502ae4ff79d4a98a8276a81175
Paulo Duarte <psmduarte@live.com>
2018-08-18 16:10:44 +0100

    Envio de imagem pelo endpoint

--- src/main/java/com/pauloduarte/cursomc/resources/ClienteResource.java
+++ src/main/java/com/pauloduarte/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pauloduarte.cursomc.domain.Cliente;
@@ -41,7 +42,8 @@ public class ClienteResource {
 	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
 		Cliente obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
@@ -78,5 +80,11 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicure(@RequestParam(name="file")MultipartFile file){
+		URI uri = service.uploadProfilePicture(file);  
+		return ResponseEntity.created(uri).build();
 	}
 }
